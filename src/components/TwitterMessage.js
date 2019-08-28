df0c085f012acec1cc86e26448c18a267c90a6ff
Chris Philbin <chris@chrisphilbin.net>
2019-08-28 14:19:40 -0400

    Done.

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -3,15 +3,26 @@ import React from "react";
 class TwitterMessage extends React.Component {
   constructor() {
     super();
+    //super() is necessary in order to use 'this' 
 
-    this.state = {};
+    this.state = { message: ''};
+    //set the state as a blank string
+  }
+
+  updateMessage = event => {
+    this.setState({
+      message: event.target.value
+      //method #updateMessage will override/add to the string stored as the value for the key 'message'
+    })
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message"
+         onChange={event => this.updateMessage(event)} value={this.state.message} />
+         {this.props.maxChars-this.state.message.length}
       </div>
     );
   }
